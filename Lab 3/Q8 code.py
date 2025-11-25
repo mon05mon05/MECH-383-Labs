42430fdeaece52f85fe48dfe1eaa44b7b8d93973
searchang <stephenchang1512@gmail.com>
2025-11-24 23:24:56 -0500

    trimmed Q8 .csv, modified Q8 code, finished Q6 and Q8 on report, included .png for Q8

--- Lab 3/Q8 code.py
+++ Lab 3/Q8 code.py
@@ -1,5 +1,6 @@
 ## Display your graphs, functions and calculations here
 ## Use print() to display your final answer
+# to convert counts to output shaft degrees/second: (counts difference) * (sample rate) * 360 / 5281
 
 import pandas as pd
 import numpy as np
@@ -7,9 +8,7 @@ import matplotlib.pyplot as plt
 
 Q8_logs = pd.read_csv("Q8 logs.csv")
 Q8_pos_and_encoders = Q8_logs.iloc[:,2:5].to_numpy()
-Q8_pos_and_encoders[:,0] = Q8_pos_and_encoders[:,0] / 2.0
-print("Q8 file", Q8_logs)
-print("Q8 pos and encoder values", Q8_pos_and_encoders)
+Q8_pos_and_encoders[:,0] = Q8_pos_and_encoders[:,0] / 2.0 # to compensate for 2x Arduino counting
 
 sample_rate = 14 # Hz
 pos = []
@@ -26,11 +25,13 @@ for i in range(len(Q8_pos_and_encoders)-1):
     counts.append(i+1)
 
 fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(12,8))
-ax[0].plot(counts[:100], EncoderA[:100])
-ax[0].plot(counts[:100], EncoderB[:100])
+ax[0].plot(counts, EncoderA)
+ax[0].plot(counts, EncoderB)
 ax[0].set_xlabel('Sample Count')
+ax[0].set_ylabel('Encoder Values')
+ax[0].set_title('Varying Encoder Values')
 ax[1].plot(counts, rot_speed)
 ax[1].set_xlabel('Sample Count')
 ax[1].set_ylabel('output shaft angular speed (degrees/second)')
-
+ax[1].grid()
 plt.show()
